ba438dae267026d07baf2888c2182232b4397f64
christmaskid <chengyt2001@gmail.com>
2025-04-23 16:06:04 +0800

    Add noise to inference

--- student_agent.py
+++ student_agent.py
@@ -2,22 +2,44 @@ import gym
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import COMPLEX_MOVEMENT
-from model import *
+import cv2
+import numpy as np
+import torch
+from model import DQNAgent
+import random
 
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, COMPLEX_MOVEMENT)
-state_size = (4, 84, 84) #env.observation_space.shape[0]
-action_size = env.action_space.n
-agent = DQNAgent(state_size, action_size, device="cpu")
-agent.load("q_net.pt", "target_net.pt")
+n_skip, n_stack = 8, 4
+img_size = (84, 84)
+state_size = (n_stack, *img_size) #env.observation_space.shape
+print("state_size", state_size) # (4, 84, 84)
+action_size = 12 #env.action_space.n
+print("action size: ", action_size)
+
+agent = DQNAgent(state_size, action_size, n_skip, batch_size=32, 
+                 gamma=0.99, soft_tau=0.1, update_interval=32, device="cpu")
+agent.load("q_net_debug.pt", "target_net_debug.pt")
 
 # Do not modify the input of the 'act' function and the '__init__' function. 
+frames= []
+
 class Agent(object):
     """Agent that acts randomly."""
     def __init__(self):
         self.action_space = gym.spaces.Discrete(12)
 
     def act(self, observation):
-        # return self.action_space.sample()
-        action = agent.get_action(observation)
+        if random.random() < 0.1: # Exploration
+            return self.action_space.sample()
+
+        obs = np.array(observation)
+        obs = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY) # grayscale
+        obs = cv2.resize(obs, img_size) # resize
+
+        if len(frames) == 0:
+            obs = np.stack([obs] * n_stack) # frame stack
+        else:
+            obs = np.stack(frames[-n_stack:] + [obs])
+        obs = obs.astype(np.float32) / 255.0
+        action = agent.get_action(obs, deterministic=True)
+        # print("action: ", action, flush=True)
         return action
